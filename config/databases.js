6af4a3be6ec3a3fdff9e15c7241faea741e82628
ndibe ebube <Ndibe-E>
2025-06-25 19:11:06 +0100

    Updated database

--- config/databases.js
+++ config/databases.js
@@ -5,14 +5,14 @@ dotenv.config(); // Load environment variables from .env file
 // Initialize Sequelize with database connection details
 // Use environment variables for configuration, with defaults if not set
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'studentsrecord_db',
-  process.env.DB_USER || 'root',
+  process.env.DB_NAME ,
+  process.env.DB_USER ,
   process.env.DB_PASSWORD , // Default to empty string if not set
   
   {
-    host: process.env.DB_HOST || 'localhost', // Default to localhost if not set,
-    dialect: process.env.DB_DIALECT || 'mysql',
-    port: process.env.DB_PORT || 3306, // Default MySQL port
+    host: process.env.DB_HOST , // Default to localhost if not set,
+    dialect: process.env.DB_DIALECT ,
+    port: process.env.DB_PORT  // Default MySQL port
   }
 );
 
